2f6addd484a9667c55697a3db5f13d6e411fd62d
Frankxxiang <xiangfeng20070726@163.com>
2025-02-14 10:43:47 +0800

    Feb14.1

---
+++ 25_Feb/ArrayListExample3.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class ArrayListExample3 {
+    public static void main(String[] args) {
+        ArrayList<String> shoppingList = new ArrayList<String>();
+        shoppingList.add("green card");
+        shoppingList.add("cake");
+        System.out.println(shoppingList);
+        //Quatities
+        ArrayList<Integer> Quantities = new ArrayList<Integer>();
+        Quantities.add(20);
+        Quantities.add(2);
+        System.out.println(Quantities);
+
+    }
+}
---
+++ 25_Feb/Arraylist.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList items = new ArrayList();
+        items.add(14);
+        items.add("jack");
+    }
+    
+
+}
