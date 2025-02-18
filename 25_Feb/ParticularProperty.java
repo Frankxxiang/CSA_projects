27a585df007a8b90fb084efa2dce2610b98acccd
Frankxxiang <xiangfeng20070726@163.com>
2025-02-18 16:43:20 +0800

    2025.2.18

--- 25_Feb/ParticularProperty.java
+++ 25_Feb/ParticularProperty.java
@@ -3,17 +3,17 @@ public class ParticularProperty {
         int[] arr = {1, 2, 3, 4, 5};
         int target = 3;
         boolean found = false;
-        for (int i = 0; i < arr.length; i++) {   // loop through the array
-            if (arr[i] == target) {   // if the current element is the target
-                found = true;   // set the flag to true
-                break;   // exit the loop
+        for (int i = 0; i < arr.length; i++) {  
+            if (arr[i] == target) {  
+                found = true;   
+                break;   
             }
-        if (found) {   // if the target was found
+        }
+        if (found) { 
             System.out.println("The target " + target + " was found in the array.");
-        } else {   // if the target was not found
+        } else {  
             System.out.println("The target " + target + " was not found in the array.");
         }
-        }
+        
     }
 }
-//  Determine atleast one element has a particular property :
